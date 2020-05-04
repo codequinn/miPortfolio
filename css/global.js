63b0fb4bfced2061ee6cff532b8181900f226655
hikky08 <ikrambabs@gmail.com>
2020-05-04 13:48:46 +0200

    My portfolio Commit

---
+++ css/global.js
@@ -0,0 +1,12 @@
+touch css/global.js
+
+npm install styled-components --save
+import { createGlobalStyle } from 'styled-components';
+
+// the `theme` object is comming from our ./themes.js file
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text}
+  }
+`
---
+++ css/theme.js
@@ -0,0 +1,12 @@
+
+mkdir css && touch css/themes.js
+
+export const lightTheme = {
+  body: '#FFFFFF',
+  text: '#363537',
+}
+
+export const darkTheme = {
+  body: '#101010',
+  text: '#FAFAFA',
+}
